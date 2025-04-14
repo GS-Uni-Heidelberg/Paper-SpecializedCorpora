c016c17e12688d33d5d7aeeba08259053aa3c8f3
Bruno Brocai <bruno-brocai@gmx.de>
2025-04-14 16:10:39 +0200

    Add function that calculates keyness for all words in the corpus

--- RQTR/src/metrics/keyness.py
+++ RQTR/src/metrics/keyness.py
@@ -2,6 +2,9 @@ import numpy as np
 import math
 from scipy import stats
 from ..corpus import FrequencyCorpus
+from typing import Callable
+import pandas as pd
+import sys
 
 
 def percent_difference(cont_table):
@@ -152,3 +155,87 @@ def corpus_to_contingency(
     )
 
     return contingency_table
+
+
+def keyword_list(
+    study_corpus: FrequencyCorpus,
+    ref_corpus: FrequencyCorpus,
+    metric: str | Callable,
+    ngram_len: int = 1,
+    min_docs: int = 1,
+    min_freq: int = 3,
+    **kwargs
+):
+    """Calculate the keyness of all ngrams in the study corpus.
+
+    Parameters:
+        study_corpus (FrequencyCorpus): The study corpus.
+        ref_corpus (FrequencyCorpus): The reference corpus.
+        metric (str or callable): The metric to use for keyness calculation.
+        ngram_len (int): The length of the n-grams to study
+            (1: study words, 2: bigrams, etc.).
+            Default is 1.
+        min_docs (int): The minimum number of documents
+            a word must appear in to be included in the results.
+            Default is 1.
+        min_freq (int): The minimum frequency a word must have
+            to be included in the results.
+            Default is 3.
+        **kwargs: Additional arguments to pass to the contigency_table
+            function, e.g. smoothing.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the keyness scores
+            and the corresponding ngrams.
+    """
+
+    if isinstance(metric, str):
+        metric_function = string_to_function(metric)
+    else:
+        metric_function = metric
+
+    keynesses = {}
+    for word in study_corpus.get_ngrams(ngram_len):
+        if study_corpus.ngram_doccounts[ngram_len][word] < min_docs:
+            continue
+        if study_corpus.ngram_counts[ngram_len][word] < min_freq:
+            continue
+        cont_table = corpus_to_contingency(
+            word,
+            study_corpus,
+            ref_corpus,
+            **kwargs
+        )
+
+        keyness_score = metric_function(cont_table)
+        keynesses[word] = keyness_score
+
+    df = pd.DataFrame(keynesses.items(), columns=['Word', 'Keyness'])
+    df = df.sort_values(by='Keyness', ascending=False)
+
+    return df
+
+
+def string_to_function(function_name: str) -> Callable:
+    """Take a string and find the corresponding function in this module.
+
+    Parameters:
+        function_name (str): The name of the function to find.
+
+    Returns:
+        Callable: The function corresponding to the string.
+    """
+    # Get the current module
+    current_module = sys.modules[__name__]
+
+    # Check if the function name exists in the current module
+    if hasattr(current_module, function_name):
+        func = getattr(current_module, function_name)
+        if callable(func):
+            return func
+        else:
+            raise ValueError(
+                f"{function_name} exists in the module but is not callable."
+            )
+    else:
+        raise ValueError(f"Function {function_name} not found in module.")
